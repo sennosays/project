d999f7277d13b71dbc9bb5392eccdc2c48b42226
sennosays <nbs5044@psu.edu>
2014-04-17 23:36:41 -0400

    added parallel MC
    added a file to test a parallel version of the crank diffusion solver.
    Also implemented a serial version which uses map. Ran some timing and
    the parallel version does better! Would like to get the explicit solver
    done

--- diffusion_solvers.jl
+++ diffusion_solvers.jl
@@ -166,3 +166,69 @@ function MC_crank_solver(u0,xi::Float64,xf::Float64,tf::Float64,D::Float64,nx::I
 
     return solution, x; 
 end
+
+function map_MC_crank_solver(u0,xi::Float64,xf::Float64,tf::Float64,D::Float64,nx::Int = 50,num_walkers::Int = 1000)
+    @assert(xf>xi); 
+    @assert(tf>0.0); 
+    @assert(D > 0.0); 
+
+    x = linspace(xi,xf,nx+2); 
+    dx = x[2] - x[1]; 
+    @assert(dx == (xf-xi)/(nx+1));
+    nt = iceil(tf*D/dx^2); 
+    dt = tf/nt; 
+    lambda = dt*D/dx^2;
+    @assert(lambda < 1.0); 
+    
+    initial_condition = map(u0,x);
+    solution = Array(Float64,nx+2);  
+    probs = zeros(Float64,nx+2); 
+    probability = (1.0-lambda)/(1.0+lambda); 
+    solution[1] = 0.0; 
+    solution[end] = 0.0; 
+
+    solution[2:end-1] = map(a -> sum(initial_condition.*random_walker(a,1,nx+2,nt,num_walkers,probability)),2:nx+1);
+
+  
+
+    return solution, x; 
+end
+
+function random_walker(initial_position::Int, left_bound::Int, right_bound::Int, initial_time::Int,n_walkers::Int,this_probability::Float64)
+	 @assert(left_bound < initial_position < right_bound);
+	 @assert(initial_time > 1); 
+	 @assert(0.0 < this_probability < 1.0); 
+	 counts = zeros(Int, right_bound-left_bound + 1); 
+	 boundary_hits = 0; 
+
+	 for n in 1:n_walkers
+	    position_index = initial_position; 
+            time_index = initial_time; 
+            
+            while((time_index > 0) && (position_index > left_bound) && (position_index < right_bound))
+                if(rand() < this_probability)
+                    time_index -= 1;
+                else 
+                    r = rand(); 
+                    if r < 0.25 
+                        position_index += 1; 
+                    elseif r < 0.5 
+                        position_index -= 1;
+                    elseif r < 0.75
+                        position_index += 1; 
+                        time_index -= 1; 
+                    else
+                        position_index -= 1;
+                        time_index -= 1; 
+                    end
+                end
+            end
+            if time_index > 0
+                boundary_hits += 1; 
+            else
+                counts[position_index] += 1; 
+            end
+        end
+	@assert(sum(counts) + boundary_hits == n_walkers);
+        return counts./n_walkers; 
+end
